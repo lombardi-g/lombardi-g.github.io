4373a3edc864dbae8b7b16a0d7e5be34942dbd4c
lombardi-g <gabriellomba@gmail.com>
2023-10-06 14:48:26 -0300

    Presentation screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { LogoMainComponent } from './logo-main/logo-main.component';
+import { SubTitleComponent } from './sub-title/sub-title.component';
+import { ArrowsComponent } from './arrows/arrows.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    LogoMainComponent
+    LogoMainComponent,
+    SubTitleComponent,
+    ArrowsComponent,
+    MainHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/arrows/arrows.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arrows',
+  templateUrl: './arrows.component.html',
+  styleUrls: ['./arrows.component.css']
+})
+export class ArrowsComponent {
+
+}
