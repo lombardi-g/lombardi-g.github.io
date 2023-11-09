b43ae606e2a8810e013ed0634f18a6db2a414c9e
lombardi-g <gabriellomba@gmail.com>
2023-11-09 10:20:56 -0300

    Created education component

---
+++ src/app/education-container/education-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-container',
+  templateUrl: './education-container.component.html',
+  styleUrls: ['./education-container.component.css']
+})
+export class EducationContainerComponent {
+
+}
