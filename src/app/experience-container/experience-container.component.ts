d8e44110c2705f230b4c967009c9ae6ee753547e
lombardi-g <gabriellomba@gmail.com>
2023-11-26 22:06:36 -0300

    Initial experience script

--- src/app/experience-container/experience-container.component.ts
+++ src/app/experience-container/experience-container.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class ExperienceContainerComponent {
 
+
+private applyTextOnHover = function(hoverElem: HTMLElement, textElem: HTMLElement, eventType: string, text: string)
+{hoverElem.addEventListener(eventType,()=>{textElem.innerHTML = text})};
+
+ngOnInit(){
+
+var experienceHeaderText = document.getElementById('skills-header')!;
+
+// no hover
+var expColumns = document.getElementsByTagName('div');
+for(var i = 0; i < expColumns.length; i++) {
+  this.applyTextOnHover(expColumns[i], experienceHeaderText, 'mouseout', 'Click to see LinkedIn page');
+}
+
+// hover over skills
+var spprospektCo = document.getElementById('spprospekt')!;
+this.applyTextOnHover(spprospektCo, experienceHeaderText, 'mouseover', '');
+
+var mrvCo = document.getElementById('mrv')!;
+this.applyTextOnHover(mrvCo, experienceHeaderText, 'mouseover', 'HTML');
+
+var figueirenseFc = document.getElementById('figueirense')!;
+this.applyTextOnHover(figueirenseFc, experienceHeaderText, 'mouseover', '');
+
+}
 }
